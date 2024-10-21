e84badc819677a0903032961d89093efd00ca683
Johny Marques <johnymarquesgon@gmail.com>
2024-10-21 11:20:30 -0300

    ainda falta muito

---
+++ main.js
@@ -0,0 +1,17 @@
+let nome = document.getElementById('nome').value;
+let ddd = document.getElementById('ddd').value;
+let numCompleto = document.getElementById('numCompleto').value;
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+ddd = Number(ddd);
+numCompleto = Number(numCompleto);
+
+
+const resultado = document.getElementById('resultado');
+
+
+
+
+}
